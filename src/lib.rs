be3a542a8214bc38dc3ad47e8832cc3c5a77fbf4
Blake Hildebrand <baugusthildebrand@gmail.com>
2026-04-12 13:54:36 -0400

    initial setup of slots

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod slot;
 use bevy::{prelude::*, shader::ShaderRef};
 pub use material::{VatMaterial, VatMaterialExtension, VatSettings};
 
+use crate::slot::VatSlotBuffers;
+
 pub fn vat_vertex_shader() -> ShaderRef {
     "shaders/vat.wgsl".into()
 }
@@ -13,6 +15,12 @@ pub struct VatPlugin;
 
 impl Plugin for VatPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(MaterialPlugin::<VatMaterial>::default());
+        app.add_plugins(MaterialPlugin::<VatMaterial>::default())
+            .init_resource::<VatHandler>();
     }
+}
+
+#[derive(Resource, Default)]
+pub struct VatHandler {
+    slot_buffers: VatSlotBuffers,
 }
--- src/slot.rs
+++ src/slot.rs
@@ -1,1 +1,64 @@
 mod allocator;
+
+use std::collections::HashMap;
+
+use crate::{VatMaterial, remap_info::AnimationClip, slot::allocator::SlotAllocator};
+
+use bevy::prelude::*;
+
+#[derive(Default)]
+pub(crate) struct VatSlotBuffers {
+    buffers: HashMap<Handle<VatMaterial>, VatSlotBuffer>,
+}
+
+impl VatSlotBuffers {
+    pub fn allocate_slot(&mut self, mat_handle: Handle<VatMaterial>) -> u32 {
+        let buffer = self.buffers.entry(mat_handle).or_default();
+
+        let slot_id = buffer.allocator.allocate();
+
+        let slot = VatSlot::default();
+        buffer.buffer.push(slot);
+
+        slot_id
+    }
+
+    // TODO: actually handle errors here
+    pub fn update_slot(
+        &mut self,
+        mat_handle: Handle<VatMaterial>,
+        slot_id: u32,
+        time_offset: f32,
+        animation_clip: AnimationClip,
+    ) {
+        let buffer = self.buffers.get_mut(&mat_handle).unwrap();
+        let slot = buffer.buffer.get_mut(slot_id as usize).unwrap();
+
+        slot.time_offset = time_offset;
+        slot.clip_start_frame = animation_clip.start_frame as f32;
+        slot.clip_frame_count = animation_clip.frame_count() as f32;
+    }
+}
+
+#[derive(Default)]
+struct VatSlotBuffer {
+    buffer: Vec<VatSlot>,
+    allocator: SlotAllocator,
+}
+
+#[derive(Default)]
+pub(crate) struct VatSlot {
+    pub time_offset: f32,
+    pub clip_start_frame: f32,
+    pub clip_frame_count: f32,
+}
+
+impl VatSlot {
+    pub fn new(time_offset: f32, clip_start_frame: f32, clip_frame_count: f32) -> Self {
+        Self {
+            time_offset,
+            clip_start_frame,
+            clip_frame_count,
+        }
+    }
+}
